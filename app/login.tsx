c999aa38517bb9905e6427d6660fdcf07b9b55c6
andresjesse <andres.porfirio.tablet@gmail.com>
2023-10-09 17:41:22 -0300

    configured expo router (#5)

---
+++ app/login.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Tela de Login!</Text>
+      <Text>Username</Text>
+      <Text>Password</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
